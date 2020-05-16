2124cb6aeb97c8dd03695945a385aa70caea321d
kIvy1189 <kevindivy@gmail.com>
2020-05-16 09:49:04 -0500

    Added initial questions for user information, began work on project details function

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # readme-maker
+
+This is a line\This should be another line.
--- index.js
+++ index.js
@@ -1,16 +1,65 @@
-// array of questions for user
-const questions = [
-
-];
+const inquirer = require("inquirer");
 
 // function to write README file
 function writeToFile(fileName, data) {
 }
 
-// function to initialize program
-function init() {
+// function to collect user data
+const userData = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'github',
+            message: 'Enter your GitHub username',
+            validate: githubInput => {
+                if (githubInput) {
+                    return true;
+                } else {
+                    console.log('You must enter your GitHub username to continue.');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Please enter your email address',
+            validate: emailInput => {
+                if (emailInput) {
+                    return true;
+                } else {
+                    console.log('You must enter your email address to continue.');
+                    return false;
+                }
+            }
+        }
+    ]);
+};
 
+//function to collect data about user's project
+const projectData = projectDetails => {
+    console.log(`
+    ================================================
+    Please answer some questions about your project.
+    ================================================
+    `);
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the title of your project?',
+            validate: titleInput => {
+                if (titeInput) {
+                    return true;
+                } else {
+                    console.log('You must give your project a title.');
+                    return false;
+                }
+            }            
+        }
+    ])
 }
 
 // function call to initialize program
-init();
+userData()
+    .then(projectData);
